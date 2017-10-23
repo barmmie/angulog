febe2191140479073ff9ac3d7c91157c92910dbc
INTERSWITCHNG\opeyemi.aina <opeyemi.aina@interswitchgroup.com>
2017-10-23 07:39:37 +0100

    fix environment ifile issues

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import { Headers, RequestOptions, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { HttpClient } from '@angular/common/http';
+import { environment } from './../../../environments/environment';
 
 @Injectable()
 export class UserService {
-    private url: string = 'http://larlog.app/api';
     
     constructor(private http: HttpClient) { }
 
     create(user) {
-        return this.http.post(`${this.url}/register`, user);
+        return this.http.post(`${environment.apiUrl}register`, user);
     }
 }
